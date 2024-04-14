b179df945792fba3bdbdff9b32d06ea0924fe008
Bruno Coghlan <brunocoghlan@gmail.com>
2024-04-14 17:54:10 -0400

    Acomodar por stylos

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,14 +5,14 @@ import IconHeart from './IconHeart'
 const Gallery = () => {
   const { photos, liked } = useContext(DataContext)
   return (
-    <div className='gallery grid-columns-5 p-3'>
+    <div className='gallery grid-columns-5 p-2'>
       {photos?.map((e) => {
         return (
-          <figure onClick={liked} key={e.id} className='photo'>
+          <div onClick={liked} key={e.id} className='photo'>
             <img id={e.id} src={e.src.tiny} alt={e.alt} />
             <IconHeart filled={e.liked} />
-            <figcaption className='text-like'>{e.alt}</figcaption>
-          </figure>
+            <p className='text-like'>{e.alt}</p>
+          </div>
         )
       })}
     </div>
